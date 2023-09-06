212017426a88ec459e81b9700ddcab55bf712a41
Jean Wang <weizi.wang75@gmail.com>
2023-09-06 15:22:23 +0800

    no message

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <CardA></CardA>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import CardA from '@/components/CardA.vue'
+
+export default {
+  components: {
+    CardA
+  }
+}
+</script>
